7da231bb28d2c589deb6483e96558b2e1afbc3ef
mustafaemirhanyildiz <mustafaemirhanyildiz@hotmail.com>
2024-06-02 17:57:32 +0300

    refactor: Sort recipe steps by step number in RecipeSteps component

--- src/app/(mainPages)/recipedetail/components/recipeSteps.tsx
+++ src/app/(mainPages)/recipedetail/components/recipeSteps.tsx
@@ -15,10 +15,10 @@ const RecipeSteps = ({ meal }: RecipeStepsProps) => {
     return (
         <div className='w-full'>
             {
-                meal?.recipeSteps.map((step: any, index: number) => (
-                    <div key={index} className='flex gap-4 mt-4'>
+                meal?.recipeSteps?.sort((a:any, b:any) => a.stepNumber - b.stepNumber).map((step: any) => (
+                    <div key={step?.stepNumber} className='flex gap-4 mt-4'>
                         <div className='flex items-center justify-center w-10 h-10 bg-red-100 rounded-full'>
-                            <span>{index + 1}</span>
+                            <span>{step?.stepNumber}</span>
                         </div>
                         <div className='flex flex-col gap-2 items-center justify-center'>
                             <span className='text-sm'>{step.description}</span>
